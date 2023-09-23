a83bf7da8c2f2bc4359da5d0304cd387593fc351
hidaya <hidaya.regragui@gmail.com>
2023-09-23 04:41:42 -0700

    task6

---
+++ sub.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+/**
+ * sub -  subtracts the top element of the stack from
+ * the second top element of the stack
+ * @stack: stack
+ * line_number: line number
+ */
+void sub(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->n -= (*stack)->n;
+	pop(stack, line_number);
+}
