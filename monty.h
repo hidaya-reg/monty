4e7bf9b86b7bae6539a2e8afcf82f72e5c4a84bd
hidaya <hidaya.regragui@gmail.com>
2023-09-23 04:34:00 -0700

    task4

--- monty.h
+++ monty.h
@@ -47,5 +47,6 @@ int is_integer(const char *str);
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
 #endif
 
