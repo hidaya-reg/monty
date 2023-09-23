786ccbca6b7bcee66a6107bf938f04b95613f496
hidaya <hidaya.regragui@gmail.com>
2023-09-23 01:54:23 -0700

    task1

--- monty.h
+++ monty.h
@@ -44,5 +44,6 @@ void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void free_stack(stack_t *stack);
 int is_integer(const char *str);
+void pint(stack_t **stack, unsigned int line_number);
 #endif
 
