550130fcaaac9291ed70a18076174c3998b28e30
hidaya <hidaya.regragui@gmail.com>
2023-09-23 01:56:29 -0700

    task1

--- pint.c
+++ pint.c
@@ -1,4 +1,10 @@
 #include "monty.h"
+/**
+ * pint - print first elt in stack
+ * @stack: A pointer to the head of the stack.
+ * @line_number: The current line number in the Monty file
+ * Return: void
+ */
 void pint(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL)
