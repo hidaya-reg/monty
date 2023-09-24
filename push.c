a3ddba39fa0b54efb672dbacde96594a77feffeb
hidaya <hidaya.regragui@gmail.com>
2023-09-23 23:32:17 -0700

    task 11: trat negative values

--- push.c
+++ push.c
@@ -13,7 +13,7 @@ void push(stack_t **stack, unsigned int line_number)
 	int value;
 
 	token = strtok(NULL, " \t\n");
-	if (token == NULL || !is_integer(token))
+	if (token == NULL)
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
